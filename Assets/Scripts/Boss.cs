d6c4a1e48731159154083cc1975d4e15004975b7
tanabe38 <tanabetanabe38@gmail.com>
2022-07-16 17:16:59 +0900

    その23 完了

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Boss : CharacterBase
+{
+    public void Kill()
+    {
+        Object.Destroy(gameObject);
+    }
+}
